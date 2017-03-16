a2bb2b0ece8451973082b1da270d9996a4e47f98
crowoy <crowoy@gmail.com>
2017-03-16 12:56:50 +0000

    Mild modifications - made so it can work with multiple sources and output directories

--- FileDownloader.py
+++ FileDownloader.py
@@ -3,44 +3,67 @@ import urllib
 import requests
 from bs4 import BeautifulSoup
 
-originUrl = 'https://www.sfu.ca/'
-extensions = ['xls']
-searchDepth = 10
-outputDirectory = './download/'
+urls =  [
+        'http://ec.europa.eu/eurostat/',
+        'https://www.sfu.ca/',
+        'http://www.fao.org/',
+        'https://data.humdata.org/',
+        'https://world.openfoodfacts.org/'
+        ]
+extensions = ['xls', 'csv']
+searchDepth = 5
+outputDirectory =   [
+                    './download_eurostat/',
+                    './download_sfu/',
+                    './download_fao/',
+                    './download_hdx/',
+                    './download_off/'
+                    ]
 
 def downloadFromUrl(originUrl, extensions, depth, directory):
     downloadCount = 0
     urls = set()
     urls.add(originUrl)
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    for i in range(depth):
-        print("Depth: " + str(i) + " - Pages: " + str(len(urls)) + " - Downloads:  " + str(downloadCount))
-        for url in urls.copy():
-            source_code = requests.get(url)
-            plain_text = source_code.text
-            soup = BeautifulSoup(plain_text, "lxml")
+        for i in range(depth):
+            for url in urls.copy():
+                print('Depth: ' + str(i) + '/' + str(searchDepth) + ' - Pages: ' + str(len(urls)) + ' - Downloads:  ' + str(downloadCount))
+                source_code = requests.get(url)
+                plain_text = source_code.text
+                soup = BeautifulSoup(plain_text, 'lxml')
 
-            for link in soup.findAll('a'):
-                href = url.rsplit('/',1)[0] + '/' + link.get('href')
+                for link in soup.findAll('a'):
+                    if link.get('href') == None:
+                        href = str(url.rsplit('/',1)[0])
+                    else :
+                        href = str(url.rsplit('/',1)[0]) + '/' + str(link.get('href'))
 
-                if any(ext in href for ext in extensions):
-                    fileName = href.rsplit('/',1)[1]
-                    filename = fileName.decode('iso-8859-1')
-                    fileName = urllib.unquote(fileName)
+                    if any(ext in href for ext in extensions):
+                        fileName = href.rsplit('/',1)[1]
+                        filename = fileName.decode('iso-8859-1')
+                        fileName = urllib.unquote(fileName)
 
-                    fullPath = directory + fileName
+                        fullPath = directory + fileName
 
-                    if not os.path.exists(fullPath):
-                        urllib.urlretrieve (href, fullPath)
-                        downloadCount += 1
-                        print(fileName)
-                else:
-                    urls.add(href)
+                        if not os.path.exists(fullPath):
+                            urllib.urlretrieve (href, fullPath)
+                            downloadCount += 1
+                            # print(fileName)
+                    else:
+                        urls.add(href)
+    except Exception:
+        print(url.rsplit('/',1)[0] + ' ---------- ' + link.get('href'))
+        pass
 
-    print("Pages visited: " + len(urls))
-    print("Files downloaded: " + downloadCount)
+    print('Pages visited: ' + str(len(urls)))
+    print('Files downloaded: ' + str(downloadCount))
 
-downloadFromUrl(originUrl, extensions, searchDepth, outputDirectory)
+for i in range(len(urls)):
+    url = urls[i]
+    directory = outputDirectory[i]
+    print('URL: ' + str(url) + ' - Directory: ' + str(directory) + ' - Depth: ' + str(searchDepth))
+    downloadFromUrl(url, extensions, searchDepth, directory)
